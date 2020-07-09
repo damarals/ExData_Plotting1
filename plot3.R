e0c5cbe0a435750ff6349ff12154c2610b0e6eef
Daniel Amaral <silva.daniel86@gmail.com>
2020-07-09 12:34:25 -0300

    png3 and plot3 file created

--- plot3.R
+++ plot3.R
@@ -1,5 +1,6 @@
 # libraries
 require(utils)
+require(lubridate)
 
 # download, load data and subset
 fileUrl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
@@ -12,7 +13,8 @@ if(file.exists('./Eletric_data.zip')) {
   # Delete original Zip file if it exists
   invisible(file.remove('./Eletric_data.zip'))
 }
-data <- read.table('household_power_consumption.txt', header = T, sep = ';')
+data <- read.table('household_power_consumption.txt', 
+                   header = T, sep = ';',stringsAsFactors = F)
 data <- subset(data, Date %in% c('1/2/2007', '2/2/2007'))
 
 # Delete data file .txt
@@ -20,7 +22,24 @@ if(file.exists('./household_power_consumption.txt')) {
   invisible(file.remove('./household_power_consumption.txt'))
 }
 
-# Make Plot 1
+# Create the png
+png('./plot3.png') # default is 480px X 480px
+
+# Make Plot 3
+with(data, plot(dmy(Date) + hms(Time), as.numeric(Sub_metering_1), 
+                xlab = '', type = 'l',
+                ylab = 'Energy sub metering'))
+with(data, lines(dmy(Date) + hms(Time), as.numeric(Sub_metering_2),
+                 col = 'red'))
+with(data, lines(dmy(Date) + hms(Time), as.numeric(Sub_metering_3),
+                 col = 'blue'))
+with(data, legend('topright', legend=c("Sub_metering_1", 
+                                       "Sub_metering_2",
+                                       "Sub_metering_3"),
+                  col=c("black", "red", "blue"), lty = 1))
+
+# Close png file
+dev.off()
 
 
 
