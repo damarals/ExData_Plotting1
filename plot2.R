592ad8cee8c30cd3d81f2b5c5e90bbb9c35cec49
Daniel Amaral <silva.daniel86@gmail.com>
2020-07-09 12:23:16 -0300

    png2 and plot2 file created

--- plot2.R
+++ plot2.R
@@ -1,5 +1,6 @@
 # libraries
 require(utils)
+require(lubridate)
 
 # download, load data and subset
 fileUrl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
@@ -12,7 +13,8 @@ if(file.exists('./Eletric_data.zip')) {
   # Delete original Zip file if it exists
   invisible(file.remove('./Eletric_data.zip'))
 }
-data <- read.table('household_power_consumption.txt', header = T, sep = ';')
+data <- read.table('household_power_consumption.txt', 
+                   header = T, sep = ';',stringsAsFactors = F)
 data <- subset(data, Date %in% c('1/2/2007', '2/2/2007'))
 
 # Delete data file .txt
@@ -20,7 +22,16 @@ if(file.exists('./household_power_consumption.txt')) {
   invisible(file.remove('./household_power_consumption.txt'))
 }
 
-# Make Plot 1
+# Create the png
+png('./plot2.png') # default is 480px X 480px
+
+# Make Plot 2
+with(data, plot(dmy(Date) + hms(Time), as.numeric(Global_active_power), 
+                xlab = '', type = 'l',
+                ylab = 'Global Active Power (kilowatts)'))
+
+# Close png file
+dev.off()
 
 
 
